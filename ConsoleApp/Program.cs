d31025931297b51e98c9e9e74bf71e3fca223bae
ivanovicdenis <ivanovicdenis85@gmail.com>
2020-12-04 17:57:52 +0100

    Rjesavanje konflikata

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,7 +6,11 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+<<<<<<< HEAD
             Console.WriteLine("Pozdrav iz aplikacije od Denis Ivanovic broj idnexa 00000");
+=======
+            Console.WriteLine("Pozdrav iz aplikacije od Denis Ivanovic");
+>>>>>>> 6c70d1e (Kreiranje ConsoleApp)
         }
     }
 }
