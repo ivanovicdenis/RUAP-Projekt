2cde07214d3808120b4ea42a88fec46f54044be5
ivanovicdenis <ivanovicdenis85@gmail.com>
2020-12-04 17:57:52 +0100

    Kreiranje ConsoleApp

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,11 +6,7 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            Console.WriteLine("Pozdrav iz aplikacije od Denis Ivanovic broj idnexa 00000");
-=======
             Console.WriteLine("Pozdrav iz aplikacije od Denis Ivanovic");
->>>>>>> 6c70d1e (Kreiranje ConsoleApp)
         }
     }
 }
